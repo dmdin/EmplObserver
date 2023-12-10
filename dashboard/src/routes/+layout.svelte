ba64ce96905e5deb76f689c5a11d8ca629b4ea37
Dmitriy Din <dmitriy1d01@gmail.com>
2023-12-11 00:02:04 +0300

    Init

---
+++ dashboard/src/routes/+layout.svelte
@@ -0,0 +1,7 @@
+<script>
+  import "../app.css";
+</script>
+
+<main class="w-full p-2">
+  <slot />
+</main>
---
+++ dashboard/src/routes/+page.svelte
@@ -0,0 +1,5 @@
+
+<div class="w-full h-screen flex flex-col items-center justify-center">
+  <h1 class="text-5xl font-black my-7">Start here</h1>
+  <button class="btn btn-xl btn-primary">Say hello!</button>
+</div>
