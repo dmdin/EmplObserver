4c563a89a2ffe376a8a782f1120f0f8d92088e5b
Комлев Данила <komlevdanila742@gmail.com>
2023-12-11 21:00:51 +0300

    docker для подписчика событий

--- EventsSubscriber/main.py
+++ EventsSubscriber/main.py
@@ -1,12 +1,24 @@
+import os
+from dotenv import load_dotenv
 from KafkaSubscriber.KafkaSubscriber import KafkaEventsConsumer
 
+load_dotenv()
 
+kafkaHost = os.getenv('KAFKA_HOST')
+kafkaPort = os.getenv('KAFKA_PORT')
+
+kafkaServer = f'{kafkaHost}:{kafkaPort}'
 
 def consume_messages():
     try:
-        consumer = KafkaEventsConsumer("winEvents", ['localhost : 9092'])
+        consumer = KafkaEventsConsumer("winEvents", [kafkaServer])
 
         for message in consumer.consume():
             print(message)
     except Exception as err:
         print(err)
+        consume_messages()
+
+
+
+consume_messages()
