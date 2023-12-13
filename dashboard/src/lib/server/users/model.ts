69cbbcf9a8ff75dd957d4d0011d27aea48504c94
Комлев Данила <komlevdanila742@gmail.com>
2023-12-14 00:14:03 +0300

    user crud

--- dashboard/src/lib/server/users/model.ts
+++ dashboard/src/lib/server/users/model.ts
@@ -3,4 +3,19 @@ import { varchar, serial, pgTable } from "drizzle-orm/pg-core";
 export const users = pgTable('user', {
   id: serial('id').primaryKey(),
   domainName: varchar('domainName', { length: 100 }),
+  domainEmail: varchar('domainEmail', { length: 100 }),
+  password: varchar('password', { length: 100 })
 });
+
+export class updateUserModel{
+  id: number | undefined
+  domainName: string | undefined
+  domainEmail: string | undefined
+  password: string | undefined
+}
+
+export class userUpdateMessage{
+  userId: number | undefined
+  success: boolean | undefined
+  message: string | undefined
+}
--- dashboard/src/lib/server/users/rpc.ts
+++ dashboard/src/lib/server/users/rpc.ts
@@ -1,10 +1,109 @@
 import {rpc} from '@chord-ts/rpc'
 import {db} from '../db' 
-import {users} from './model'
+import {updateUserModel, userUpdateMessage, users} from './model'
+import { eq, or } from 'drizzle-orm'
 
 export class User {
   @rpc()
   async getAll() {
     return db.select().from(users)
+  }
+
+  @rpc()
+  async create(user: updateUserModel){
+
+    let response: userUpdateMessage = {
+      userId: undefined,
+      success: true,
+      message: ""
+    }
+
+    if(!user.domainEmail ||
+        !user.domainName)
+    {
+      response.message = "Доменное имя и почта обязательны для заполнения";
+      response.success = false;
+      return response;
+    }
+
+    
+    let existedUsers = await db.select().from(users)
+      .where(or(eq(users.domainName, user.domainName ?? ""), eq(users.domainEmail, user.domainEmail ?? "")));
+
+    if(existedUsers.length > 0){
+      response.message = "Пользователь с таким доменным именем или почтой уже существует";
+      response.success = false;
+      return response;
+    }
+
+    let insertResult = await db.insert(users).values(user).returning({ insertedId: users.id });
+
+    response.message = "Пользователь успешно добавлен"
+    response.userId = insertResult[0].insertedId;
+
+    return response;
+  }
+
+  @rpc()
+  async update(user: updateUserModel){
+
+    let response: userUpdateMessage = {
+      userId: undefined,
+      success: true,
+      message: ""
+    }
+
+    if(!user.domainEmail ||
+        !user.domainName)
+    {
+      response.message = "Доменное имя и почта обязательны для заполнения";
+      response.success = false;
+      return response;
+    }
+
+    if(!user.id){
+      response.message = "Id обязатьельно для запонения";
+      response.success = false;
+      return response;
+    }
+
+    let updateResult = await db.update(users)
+      .set({ domainEmail: user.domainEmail, domainName: user.domainName })
+      .where(eq(users.id, user.id))
+      .returning({ updatedId: users.id });
+
+    if(updateResult.length == 0){
+      response.message = "Пользователь с переданным Id не найден";
+      response.success = false;
+      return response;
+    }
+
+    response.userId = user.id
+    response.message = "Пользователь успешно обновлен"
+    return response;
+  }
+
+  @rpc()
+  async delete(userId: number){
+    const deletedUserIds: { deletedId: number }[] = await db.delete(users)
+      .where(eq(users.id, userId))
+      .returning({ deletedId: users.id });
+
+    let response: userUpdateMessage = {
+        userId: undefined,
+        success: true,
+        message: ""
+      }
+
+    if(deletedUserIds.length == 0){
+      response.message = "Пользователь с переданным id не найден"
+      response.success = false
+      return response
+    }
+
+    response.userId = userId
+    response.message = "Пользователь успешно удален"
+
+    return response;
   }
 }
