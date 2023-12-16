3bf914bc2c73fb88b9b1df0f404e11f459d37634
Dmitriy Din <dmitriy1d01@gmail.com>
2023-12-16 22:44:27 +0300

    Added dynamic badges

--- dashboard/src/routes/users/[userId]/ui/Badge.svelte
+++ dashboard/src/routes/users/[userId]/ui/Badge.svelte
@@ -1,20 +1,20 @@
-<script>
+<script lang="ts">
   import Icon from '@iconify/svelte'
 
   export let title = "Проработанные часы"
-  export let value = "20"
+  export let value: string | number = "20"
   export let measure = 'часов'
   export let label = "Количество часов возросло"
   export let icon = 'mdi:clock'
 </script>
 
-<div class="bg-backgroundSecondary px-3 py-4 rounded-md shadow-md">
+<div class="bg-backgroundSecondary px-3 py-4 rounded-md shadow-md flex flex-col justify-between">
   <div class="flex justify-between gap-4">
-    <span class="font-bold">{title}</span>
+    <span class="font-bold" >{title}</span>
     <Icon {icon} class="text-primary" width="20"/>
   </div>
   <div class="font-black flex items-center gap-2 my-2">
-    <span class=" text-5xl">{value}</span>
+    <span class=" text-5xl {$$props.class}">{value}</span>
     <span>{measure}</span>
   </div>
   <span>
